6d79cc1bdb691d4d3b8394e3a38dbdeec4bf01c5
Christopher Katsaros <chris.katsaros34@gmail.com>
2022-01-23 13:20:14 -0500

    Init with copied Existing Project

---
+++ FieldLevel/Controllers/FieldLevelController.cs
@@ -0,0 +1,49 @@
+﻿using FieldLevel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static FieldLevel.Misc.ClientVariables;
+
+namespace FieldLevel.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FieldLevelController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public FieldLevelController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        [Route("LatestPosts")]
+        public async Task<ActionResult<TypicodePost[]>> GetLatestPosts()
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient(Typicode.Client);
+                HttpResponseMessage result = await client.GetAsync(Typicode.Calls.Posts);
+                if (result.IsSuccessStatusCode)
+                {
+                    var jsonResult = result.Content.ReadAsStringAsync().Result;
+                    //Group the results by userId and pull only the latest Id per user
+                    var filteredPosts = TypicodePost.GroupAndMax(jsonResult);
+                    //var allPosts = JsonSerializer.Deserialize<TypicodePost[]>(jsonResult);
+                    return Ok(filteredPosts);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ FieldLevel/Models/TypicodePost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FieldLevel.Models
+{
+    public class TypicodePost
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+        public static TypicodePost[] GroupAndMax(string postsJson)
+        {
+            var allPosts = JsonSerializer.Deserialize<TypicodePost[]>(postsJson);
+            var userGroups = allPosts.GroupBy(p => (p.userId));
+            return userGroups.SelectMany(grp => grp.Where(p => p.id == grp.Max(p => p.id))).ToArray();
+        }
+    }
+}
